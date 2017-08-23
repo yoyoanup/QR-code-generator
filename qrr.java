95edbb8e5cde6565d1987955ca814f9f269ff284
yoyoanup <nanki.anup@gmail.com>
2017-08-23 08:58:15 +0530

    Update qrr.java

--- qrr.java
+++ qrr.java
@@ -1,7 +1,6 @@
 // it requires 3 software   which i am also uploading
 // if you are facing any problem , you can contact me  -> nanki.anup@gmail.com
 package gen;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -10,19 +9,17 @@ import net.glxn.qrgen.image.ImageType;
 
 
 public class Gen {
-
-    
-    public static void main(String[] args) throws Exception {
-        // TODO code application logic here
+                  public static void main(String[] args) throws Exception {
+                   // TODO code application logic here
         
         
-          String details="https://www.donationclub.info/";
-        ByteArrayOutputStream out=QRCode.from(details).to(ImageType.JPG).stream();
-// file directory where qr logo generated 
-        File f=new File("C:\\Users\\yo yo anup\\Downloads\\os\\Qr.jpg");
-        FileOutputStream fos =new FileOutputStream(f);
-        fos.write(out.toByteArray());
-        fos.flush();
+                    String details="https://www.donationclub.info/";
+                    ByteArrayOutputStream out=QRCode.from(details).to(ImageType.JPG).stream();
+                       // file directory where qr logo generated 
+                       File f=new File("C:\\Users\\yo yo anup\\Downloads\\os\\Qr.jpg");
+                      FileOutputStream fos =new FileOutputStream(f);
+                       fos.write(out.toByteArray());
+                        fos.flush();
     }
     
 }
